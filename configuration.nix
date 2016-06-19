dfe31907037154a6b896441deacccc168207c8d7
= <pkinsky@gmail.com>
2016-06-19 08:38:32 +0000

    neat tweaks, still working

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,33 @@ let zsh = "/run/current-system/sw/bin/zsh";
       rev = "1359b9966689e5afb666c2c31f5ca177006ce710";
       sha256 = "0fqxidxrjli1a02f5564y6av5jz32sh86x5yq6rpv1hhr54n521w";
     };
+    vim = "vim-conf";
+    my_vim = pkgs.vim_configurable.customize {
+      name = vim;
+      vimrcConfig.customRC = ''
+        filetype plugin indent on
+        set nocompatible
+        syntax enable
+        set background=dark
+        colorscheme solarized
+        set nostartofline
+
+        " fixes backspace, see http://stackoverflow.com/questions/5419848/backspace-doesnt-work-in-gvim-7-2-64-bit-for-windows
+        set backspace=2
+        set backspace=indent,eol,start
+
+        nnoremap <F4> :NERDTreeToggle<CR>  
+        " and some more stuff...
+      '';
+      vimrcConfig.vam.knownPlugins = pkgs.vimPlugins;
+      vimrcConfig.vam.pluginDictionaries = [ 
+        #load always
+        { names = [ "Syntastic" "ctrlp" "colors-solarized" "supertab" "nerdtree" "rainbow_parentheses"]; } 
+        #only load for scala files
+        #{ name = "vim-scala"; ft_regex = "^.php\$";} <- not found
+        #{ name = "vim-scala"; filename_regex = "^.php\$";}
+      ];
+    };
     #storeFile = fileSrc : builtins.toFile (baseNameOf fileSrc) (builtins.readFile fileSrc);
     #test = storeFile "/etc/nixos/dotfiles/test.dot"
 in {
@@ -83,11 +110,13 @@ in {
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
 
+
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     wget
-    vim
+    #vim
     binutils
     git
     chromium
@@ -95,6 +124,7 @@ in {
     zip
     xclip
     tree
+    my_vim
   ];
 
   # The NixOS release to be compatible with for stateful data such as databases.
@@ -102,6 +132,10 @@ in {
   
   programs.zsh = {
     enable = true;
+    shellAliases = {
+      v = "${vim} -g";
+      g = "git";
+    };
     enableCompletion = true;
     interactiveShellInit = ''
       source ${antigen}/antigen.zsh
